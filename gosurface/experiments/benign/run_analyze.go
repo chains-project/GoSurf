0dff5912d8d3d9022167bcc291ea6cff884be76f
carminecesarano <c.cesarano@hotmail.it>
2024-06-06 15:12:56 +0200

    var renaming

--- gosurface/experiments/benign/run_analyze.go
+++ gosurface/experiments/benign/run_analyze.go
@@ -12,11 +12,11 @@ import (
 	analysis "github.com/chains-project/capslock-analysis/gosurface/libs"
 )
 
-type PackageDetails struct {
-	PackagePath            string
+type ModuleDetails struct {
+	ModulePath             string
 	Version                string
 	InitCount              int
-	AnonymCount            int
+	GlobalVarCount         int
 	ExecCount              int
 	PluginCount            int
 	GoGenerateCount        int
@@ -27,7 +27,7 @@ type PackageDetails struct {
 	ReflectCount           int
 	ConstructorCount       int
 	InitOccurrences        []*analysis.Occurrence
-	AnonymOccurrences      []*analysis.Occurrence
+	GlobalVarOccurrences   []*analysis.Occurrence
 	ExecOccurrences        []*analysis.Occurrence
 	PluginOccurrences      []*analysis.Occurrence
 	GoGenerateOccurrences  []*analysis.Occurrence
@@ -46,12 +46,13 @@ func main() {
 		fmt.Println("Error getting current directory:", err)
 		return
 	}
-	filePath := filepath.Join(currentDir, "results.json")
+	moduleFilePath := filepath.Join(currentDir, "modules_list.json")
 
-	/* // Gather packages from libraries.io
-		var allPackages []map[string]interface{}
+	// Modules gathering from libraries.io
+	var allModules []map[string]interface{}
 
-		// Retrieve TOP x packages from libraries.io API
+	// Retrieve TOP x packages from libraries.io API
+	/*
 		for page := 1; page <= 5; page++ {
 
 			url := fmt.Sprintf("https://libraries.io/api/search?order=desc&platforms=Go&sort=dependents_count&per_page=1&page=%d&api_key=ff76aa15a1d65e44843fb94dab1ead62", page)
@@ -80,65 +81,63 @@ func main() {
 				delete(data[i], "versions")
 			}
 
-			allPackages = append(allPackages, data...)
+			allModules = append(allModules, data...)
 		}
 
 		// Print retrieved packages to JSON
-		modifiedJSON, err := json.MarshalIndent(allPackages, "", "  ")
+		modifiedJSON, err := json.MarshalIndent(allModules, "", "  ")
 		if err != nil {
 			fmt.Println("Error marshaling JSON:", err)
 			return
 		}
-		err = os.WriteFile(filePath, modifiedJSON, 0644)
+		err = os.WriteFile(moduleFilePath, modifiedJSON, 0644)
 		if err != nil {
 			fmt.Println("Error writing to file:", err)
 			return
 		}
-		fmt.Println("Modified JSON written to", filePath)
-		itemCount := len(allPackages)
+		fmt.Println("Modified JSON written to", moduleFilePath)
+		itemCount := len(allModules)
 		fmt.Println("Number of items in the JSON file:", itemCount)
-
 	*/
 
 	/* Read package paths from file*/
-	file, err := os.Open(filePath)
+	file, err := os.Open(moduleFilePath)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
 	}
 	defer file.Close()
 
-	var allPackages []map[string]interface{}
-	err = json.NewDecoder(file).Decode(&allPackages)
+	err = json.NewDecoder(file).Decode(&allModules)
 	if err != nil {
 		fmt.Println("Error decoding JSON:", err)
 		return
 	}
-	itemCount := len(allPackages)
+	itemCount := len(allModules)
 	/* **********************/
 
 	// Parse the HTML templates
-	overviewTmpl, err := template.ParseFiles("tmpl_overview.html")
+	overviewTmpl, err := template.ParseFiles("../report_tmpl/tmpl_overview.html")
 	if err != nil {
 		fmt.Println("Error parsing overview template:", err)
 		return
 	}
 
-	detailsTmpl, err := template.ParseFiles("tmpl_details.html")
+	detailsTmpl, err := template.ParseFiles("../report_tmpl/tmpl_details.html")
 	if err != nil {
 		fmt.Println("Error parsing details template:", err)
 		return
 	}
 
 	// Create the HTML files
-	overviewFile, err := os.Create("analysis_overview.html")
+	overviewFile, err := os.Create("results_overview.html")
 	if err != nil {
 		fmt.Println("Error creating overview file:", err)
 		return
 	}
 	defer overviewFile.Close()
 
-	detailsFile, err := os.Create("analysis_detail.html")
+	detailsFile, err := os.Create("results_detail.html")
 	if err != nil {
 		fmt.Println("Error creating details file:", err)
 		return
@@ -146,19 +145,19 @@ func main() {
 	defer detailsFile.Close()
 
 	// Slices to hold PackageAnalysis and PackageDetails instances
-	var packageDetails []PackageDetails
+	var moduleDetails []ModuleDetails
 
-	// Get and analyze each package
-	for i, pkg := range allPackages {
+	// Get and analyze each module
+	for i, pkg := range allModules {
 		packageManagerURL := pkg["package_manager_url"].(string)
 		latestReleaseNumber := pkg["latest_release_number"].(string)
 
 		// Construct the package import path and version
 		importPath := strings.TrimPrefix(packageManagerURL, "https://pkg.go.dev/")
 		version := "@" + latestReleaseNumber
-		fmt.Printf("[%d/%d] Analyzing package %s...\n", i+1, itemCount, importPath)
+		fmt.Printf("\n[%d/%d] Analyzing package %s...\n", i+1, itemCount, importPath)
 
-		// Get the package
+		// Get the module
 		cmd := exec.Command("go", "get", importPath+version)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
@@ -168,27 +167,36 @@ func main() {
 			continue
 		}
 
-		// Analyze the package
-		var initOccurrences, anonymOccurrences, execOccurrences, pluginOccurrences, goGenerateOccurrences, goTestOccurrences, unsafeOccurrences, cgoOccurrences, indirectOccurrences, reflectOccurrences, constructorOccurrences []*analysis.Occurrence
-		packagePath := filepath.Join(os.Getenv("GOPATH"), "pkg/mod", importPath+"@"+latestReleaseNumber)
+		// Analyze the module
+		var initOccurrences, globalVarOccurrences, execOccurrences, pluginOccurrences, goGenerateOccurrences, goTestOccurrences, unsafeOccurrences, cgoOccurrences, indirectOccurrences, reflectOccurrences, constructorOccurrences []*analysis.Occurrence
+		modulePath := filepath.Join(os.Getenv("GOPATH"), "pkg/mod", importPath+"@"+latestReleaseNumber)
 
 		// Analyze the module and its direct dependencies
-		analysis.AnalyzeModule(packagePath, &initOccurrences, analysis.InitFuncParser{})
-		analysis.AnalyzeModule(packagePath, &anonymOccurrences, analysis.AnonymFuncParser{})
-		analysis.AnalyzeModule(packagePath, &execOccurrences, analysis.ExecParser{})
-		analysis.AnalyzeModule(packagePath, &pluginOccurrences, analysis.PluginParser{})
-		analysis.AnalyzeModule(packagePath, &goGenerateOccurrences, analysis.GoGenerateParser{})
-		analysis.AnalyzeModule(packagePath, &goTestOccurrences, analysis.GoTestParser{})
-		analysis.AnalyzeModule(packagePath, &unsafeOccurrences, analysis.UnsafeParser{})
-		analysis.AnalyzeModule(packagePath, &cgoOccurrences, analysis.CgoParser{})
-		analysis.AnalyzeModule(packagePath, &indirectOccurrences, analysis.IndirectParser{})
-		analysis.AnalyzeModule(packagePath, &reflectOccurrences, analysis.ReflectParser{})
-		// analysis.AnalyzeModule(packagePath, &constructorOccurrences, analysis.ConstructorParser{})
+		direct_dependencies, err := analysis.GetDependencies(modulePath)
+		if err != nil {
+			fmt.Printf("Error getting files in module: %v\n", err)
+			return
+		}
+
+		// Analyze all the module direct dependencies
+		for _, dep := range direct_dependencies {
+			analysis.AnalyzePackage(dep, &initOccurrences, analysis.InitFuncParser{})
+			analysis.AnalyzePackage(dep, &globalVarOccurrences, analysis.GlobalVarParser{})
+			analysis.AnalyzePackage(dep, &execOccurrences, analysis.ExecParser{})
+			analysis.AnalyzePackage(dep, &pluginOccurrences, analysis.PluginParser{})
+			analysis.AnalyzePackage(dep, &goGenerateOccurrences, analysis.GoGenerateParser{})
+			analysis.AnalyzePackage(dep, &goTestOccurrences, analysis.GoTestParser{})
+			analysis.AnalyzePackage(dep, &unsafeOccurrences, analysis.UnsafeParser{})
+			analysis.AnalyzePackage(dep, &cgoOccurrences, analysis.CgoParser{})
+			analysis.AnalyzePackage(dep, &indirectOccurrences, analysis.IndirectParser{})
+			analysis.AnalyzePackage(dep, &reflectOccurrences, analysis.ReflectParser{})
+			//analysis.AnalyzePackage(dep, &constructorOccurrences, analysis.ConstructorParser{})
+		}
 
 		// Convert occurrences to JSON
 		occurrences := append(append(append(append(append(append(append(append(append(append(
 			initOccurrences,
-			anonymOccurrences...),
+			globalVarOccurrences...),
 			execOccurrences...),
 			pluginOccurrences...),
 			goGenerateOccurrences...),
@@ -200,14 +208,14 @@ func main() {
 			constructorOccurrences...)
 
 		// Count unique occurrences
-		initCount, anonymCount, execCount, pluginCount, goGenerateCount, goTestCount, unsafeCount, cgoCount, indirectCount, reflectCount, constructorCount := analysis.CountUniqueOccurrences(occurrences)
+		initCount, globalVarCount, execCount, pluginCount, goGenerateCount, goTestCount, unsafeCount, cgoCount, indirectCount, reflectCount, constructorCount := analysis.CountUniqueOccurrences(occurrences)
 
-		// Create a PackageDetails instance and append it to the slice
-		packageDetail := PackageDetails{
-			PackagePath:            packagePath,
+		// Create a ModuleDetails instance and append it to the slice
+		moduleDetail := ModuleDetails{
+			ModulePath:             modulePath,
 			Version:                latestReleaseNumber,
 			InitCount:              initCount,
-			AnonymCount:            anonymCount,
+			GlobalVarCount:         globalVarCount,
 			ExecCount:              execCount,
 			PluginCount:            pluginCount,
 			GoGenerateCount:        goGenerateCount,
@@ -218,7 +226,7 @@ func main() {
 			ReflectCount:           reflectCount,
 			ConstructorCount:       constructorCount,
 			InitOccurrences:        initOccurrences,
-			AnonymOccurrences:      anonymOccurrences,
+			GlobalVarOccurrences:   globalVarOccurrences,
 			ExecOccurrences:        execOccurrences,
 			PluginOccurrences:      pluginOccurrences,
 			GoGenerateOccurrences:  goGenerateOccurrences,
@@ -229,23 +237,23 @@ func main() {
 			ReflectOccurrences:     reflectOccurrences,
 			ConstructorOccurrences: constructorOccurrences,
 		}
-		packageDetails = append(packageDetails, packageDetail)
+		moduleDetails = append(moduleDetails, moduleDetail)
 	}
 
-	// Execute the overview template with the PackageAnalysis instances
-	err = overviewTmpl.Execute(overviewFile, packageDetails)
+	// Execute the overview template with the ModuleDetails instances
+	err = overviewTmpl.Execute(overviewFile, moduleDetails)
 	if err != nil {
 		fmt.Println("Error executing overview template:", err)
 		return
 	}
 
-	// Execute the details template with the PackageDetails instances
-	err = detailsTmpl.Execute(detailsFile, packageDetails)
+	// Execute the details template with the ModuleDetails instances
+	err = detailsTmpl.Execute(detailsFile, moduleDetails)
 	if err != nil {
 		fmt.Println("Error executing details template:", err)
 		return
 	}
 
-	fmt.Println("HTML file generated: attack_surface_analysis.html")
+	fmt.Println("HTML report generated successfully in the current directory.")
 
 }
---
+++ gosurface/gosurface.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	analysis "github.com/chains-project/capslock-analysis/gosurface/libs"
+)
+
+var (
+	initOccurrences        []*analysis.Occurrence
+	globalVarOccurrences   []*analysis.Occurrence
+	execOccurrences        []*analysis.Occurrence
+	pluginOccurrences      []*analysis.Occurrence
+	goGenerateOccurrences  []*analysis.Occurrence
+	goTestOccurrences      []*analysis.Occurrence
+	unsafeOccurrences      []*analysis.Occurrence
+	cgoOccurrences         []*analysis.Occurrence
+	indirectOccurrences    []*analysis.Occurrence
+	reflectOccurrences     []*analysis.Occurrence
+	constructorOccurrences []*analysis.Occurrence
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <module_path>")
+		return
+	}
+
+	modulePath := os.Args[1]
+
+	asciiArt := `
+                                                                                                               
+  ,ad8888ba,                 ad88888ba                               ad88                                      
+ d8"'    '"8b               d8"     "8b                             d8"                                        
+d8'                         Y8,                                     88                                         
+88              ,adPPYba,   'Y8aaaaa,    88       88  8b,dPPYba,  MM88MMM  ,adPPYYba,   ,adPPYba,   ,adPPYba,  
+88      88888  a8"     "8a    '"""""8b,  88       88  88P'   "Y8    88     ""     '8  a8"     ""  a8P_____88  
+Y8,        88  8b       d8          '8b  88       88  88            88     ,adPPPPP88  8b          8PP"""""""  
+ Y8a.    .a88  "8a,   ,a8"  Y8a     a8P  "8a,   ,a88  88            88     88,    ,88  "8a,   ,aa  "8b,   ,aa  
+  '"Y88888P"    '"YbbdP"'    "Y88888P"    '"YbbdP'Y8  88            88     '"8bbdP"Y8   '"Ybbd8"'   '"Ybbd8"'  
+                                                                                                               
+                                                                                                          "
+`
+	fmt.Println(asciiArt)
+
+	fmt.Println("GoSurface is a tool that aims to analyze the potential attack surface of open-source Go packages and modules.")
+	fmt.Println("It looks for occurrences of various features and constructs that could potentially introduce security risks.")
+	fmt.Println()
+
+	// TODO: currently only fetches direct dependencies in the module, not external dependencies
+
+	// Get direct dependencies
+	direct_dependencies, err := analysis.GetDependencies(modulePath)
+	if err != nil {
+		fmt.Printf("Error getting files in module: %v\n", err)
+		return
+	}
+	analysis.PrintDependencies(direct_dependencies)
+
+	// Analyze all the module direct dependencies
+	for _, dep := range direct_dependencies {
+		analysis.AnalyzePackage(dep, &initOccurrences, analysis.InitFuncParser{})
+		analysis.AnalyzePackage(dep, &globalVarOccurrences, analysis.GlobalVarParser{})
+		analysis.AnalyzePackage(dep, &execOccurrences, analysis.ExecParser{})
+		analysis.AnalyzePackage(dep, &pluginOccurrences, analysis.PluginParser{})
+		analysis.AnalyzePackage(dep, &goGenerateOccurrences, analysis.GoGenerateParser{})
+		analysis.AnalyzePackage(dep, &goTestOccurrences, analysis.GoTestParser{})
+		analysis.AnalyzePackage(dep, &unsafeOccurrences, analysis.UnsafeParser{})
+		analysis.AnalyzePackage(dep, &cgoOccurrences, analysis.CgoParser{})
+		analysis.AnalyzePackage(dep, &indirectOccurrences, analysis.IndirectParser{})
+		analysis.AnalyzePackage(dep, &reflectOccurrences, analysis.ReflectParser{})
+		//analysis.AnalyzePackage(dep, &constructorOccurrences, analysis.ConstructorParser{})
+	}
+
+	// Convert occurrences to JSON
+	occurrences := append(append(append(append(append(append(append(append(append(append(
+		initOccurrences,
+		globalVarOccurrences...),
+		execOccurrences...),
+		pluginOccurrences...),
+		goGenerateOccurrences...),
+		goTestOccurrences...),
+		unsafeOccurrences...),
+		cgoOccurrences...),
+		indirectOccurrences...),
+		reflectOccurrences...),
+		constructorOccurrences...)
+
+	// Print occurrences
+	// analysis.PrintOccurrences(execOccurrences)
+	// analysis.PrintOccurrences(occurrences)
+
+	// Count unique occurrences
+	initCount, globalVarCount, execCount, pluginCount, goGenerateCount, goTestCount, unsafeCount, cgoCount, indirectCount, reflectCount, constructorCount := analysis.CountUniqueOccurrences(occurrences)
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("╔═════════════════════════════════════════════════════════════════════════╗")
+	fmt.Printf("║ Attack Surface Analysis: %s	     		         ║\n", filepath.Base(strings.TrimSuffix(modulePath, "/"+filepath.Base(modulePath))))
+	fmt.Println("╠═════════════════════════════════════════════════════════════════════════╣")
+	fmt.Printf("║ init() function definitions:                                 %10d ║\n", initCount)
+	fmt.Printf("║ global var initialization with functions:                    %10d ║\n", globalVarCount)
+	fmt.Printf("║ exec function invocations:                                   %10d ║\n", execCount)
+	fmt.Printf("║ plugin dynamically loaded:                                   %10d ║\n", pluginCount)
+	fmt.Printf("║ 'go:generate' directive usage:                               %10d ║\n", goGenerateCount)
+	fmt.Printf("║ testing function definitions:                                %10d ║\n", goTestCount)
+	fmt.Printf("║ Unsafe pointers:                                             %10d ║\n", unsafeCount) // TODO: define better
+	fmt.Printf("║ C function invocations via CGO:                              %10d ║\n", cgoCount)
+	fmt.Printf("║ Indirect method calls via interfaces:                        %10d ║\n", indirectCount)
+	fmt.Printf("║ Usage of reflection:                                         %10d ║\n", reflectCount) // TODO: define better
+	fmt.Printf("║ Invocation of constructors:                                  %10d ║\n", constructorCount)
+	fmt.Println("╚═════════════════════════════════════════════════════════════════════════╝")
+}
